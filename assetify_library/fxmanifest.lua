1250b979efdfbf5f77e818cb1270987177155c19
Aviril <48655846+OvileAmriam@users.noreply.github.com>
2023-01-13 13:24:54 +0300

    Update fxmanifest.lua

--- assetify_library/fxmanifest.lua
+++ assetify_library/fxmanifest.lua
@@ -11,28 +11,22 @@ shared_scripts {
     -->> Utilities: Sandbox <<--
     "utilities/sandbox/index.lua",
     "utilities/sandbox/namespacer.lua",
-    "utilities/sandbox/vcl.lua",
-    "utilities/sandbox/table.lua",
+    "utilities/sandbox/vcl.lua", --TODO: WIP
+    "utilities/sandbox/table.lua", --TODO: WIP
     "utilities/sandbox/filesystem.lua", --TODO: WIP
     "utilities/sandbox/math/index.lua",
     "utilities/sandbox/math/quat.lua",
     "utilities/sandbox/math/matrix.lua",
-    "utilities/sandbox/string.lua",
-    "utilities/sandbox/timer.lua",
-    "utilities/sandbox/threader.lua",
-    "utilities/sandbox/networker.lua",
-    "utilities/sandbox/rest.lua",
-    "utilities/engine/syncer.lua",
-    "utilities/sandbox/datasystem.lua",
+    "utilities/sandbox/string.lua", --TODO: WIP
+    "utilities/sandbox/timer.lua", --TODO: WIP
+    "utilities/sandbox/threader.lua", --TODO: WIP
+    "utilities/sandbox/networker.lua", --TODO: WIP
+    "utilities/sandbox/rest.lua", --TODO: WIP
+    "utilities/engine/syncer.lua", --TODO: WIP
+    "utilities/sandbox/datasystem.lua"
 }
 
-client_scripts {
-    "client/main.lua",
-    "client/functions.lua",
-    "client/loops.lua",
-    "client/events.lua",
-    "client/drawtext.lua"
-}
+client_scripts {}
 
 server_scripts {
     -->> Settings <<--
