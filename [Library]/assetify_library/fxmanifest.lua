74ab5d7cdb8ad1b5c2184d1605773ec3800b2b4b
Aviril <48655846+OvileAmriam@users.noreply.github.com>
2023-01-13 14:58:58 +0300

    Update fxmanifest.lua

--- [Library]/assetify_library/fxmanifest.lua
+++ [Library]/assetify_library/fxmanifest.lua
@@ -1,8 +1,8 @@
-fx_version "cerulean"
-game "gta5"
-
+author "vStudio <crazynuggets@outlook.com>'
 description "Assetify Library"
 version "1.0.0"
+fx_version "cerulean"
+game "gta5"
 
 shared_scripts {
     -->> Settings <<--
