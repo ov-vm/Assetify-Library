0f5574b00dea92b0e6bd6a38ad24784fd7882760
Aviril <48655846+OvileAmriam@users.noreply.github.com>
2023-01-13 15:22:31 +0300

    Update test.lua

--- [Library]/assetify_library/test.lua
+++ [Library]/assetify_library/test.lua
@@ -1,4 +1,35 @@
+--[[
 print("Testing Timer, Creating a timer that executes 5 times every 1 second")
 timer:create(function()
     print("Timer Executed")
 end, 1000, 5)
+]]
+
+print("Testing Thread, Creating a thread that pauses for 2.5 seconds after every print")
+thread:create(function()
+    print("First Print")
+    print(">>> Going to sleep now")
+    sleep(2500)
+
+    print(">>> Wokeup")
+    print("Second Print")
+    print(">>> Going to sleep now")
+    sleep(2500)
+
+    print(">>> Wokeup")
+    print("Third Print")
+    print(">>> Going to sleep now")
+    sleep(2500)
+
+    print(">>> Wokeup")
+    print("Fourth Print")
+    print(">>> Going to sleep now")
+    sleep(2500)
+
+    print(">>> Wokeup")
+    print("Fifth Print")
+    print(">>> Going to sleep now")
+    sleep(2500)
+
+    print("Finished thread execution completely!")
+end):resume()
